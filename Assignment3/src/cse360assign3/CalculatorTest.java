8ca802b94dec989abb84c293909b0e44ac96b509
Alex <Alex@Alex-PC>
2016-02-23 19:16:09 -0700

    Edited test cases

---
+++ Assignment3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,46 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator test = new Calculator();
+		assertNotNull(test);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHistory() {
+		fail("Not yet implemented");
+	}
+
+
+}
